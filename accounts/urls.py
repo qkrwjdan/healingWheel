72e6b8e8e3200a7cb720e1459f67da5e2e58c0c6
qkrwjdan <madogisa12@naver.com>
2019-05-29 14:48:59 +0900

    회원가입

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('register/',register,name='register'),
     path('login/',auth_view.LoginView.as_view(template_name='registration/login.html'),name='login'),
-    path('logout/',auth_view.LogoutView.as_view(template_name='registraion/logout.html'),name='logout'),
+    path('logout/',auth_view.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
 
 ]

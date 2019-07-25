343aa6f09910aae4ee81dcd9f8fbc42dfbe20d18
qkrwjdan <madogisa12@naver.com>
2019-07-26 00:27:54 +0900

    맥북이 터질까 무섭다...

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('',index,name='index')
+    path('',views.index,name='index'),
+    path('aboutus',views.aboutus,name='aboutus'),
+    path('loc_ren',views.loc_ren,name='loc_ren'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'main/index.html',)
+
+def aboutus(request):
+    return render(request,'main/aboutus.html',)
+
+def loc_ren(request):
+    return render(request,'main/loc_ren.html',)

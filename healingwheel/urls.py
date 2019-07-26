fad3ae2081bd82d1fa39e3cd4842e22b6c538682
qkrwjdan <madogisa12@naver.com>
2019-07-26 21:43:09 +0900

    ID까지 완성요

--- healingwheel/urls.py
+++ healingwheel/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('accounts',include('accounts.urls')),
+    path('accounts/',include('accounts.urls')),
     path('',include('main.urls')),
     path('cal',include('cal.urls')),
 

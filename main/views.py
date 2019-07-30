f91fbead22b3975ae77ee2532602b820b76f485c
qkrwjdan <madogisa12@naver.com>
2019-07-30 17:02:46 +0900

    로그인이 필요해요~

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import DrivingTimeForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request,'main/index.html',)
@@ -16,6 +17,7 @@ def how_to(request):
 def contact(request):
     return render(request,'main/contact.html')
 
+@login_required
 def charge(request):
     if request.method == 'POST':
         form = DrivingTimeForm()
